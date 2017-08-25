a2e1b9a8b158cbe218414662cf818809fa0662a7
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-08-25 14:59:22 -0700

    add teacherDao class

---
+++ epicodus/src/main/java/dao/TeacherDao.java
@@ -0,0 +1,20 @@
+package dao;
+
+import models.Teacher;
+
+import java.util.List;
+
+public interface TeacherDao {
+
+    void add(Teacher teacher);
+
+    List<Teacher> getAllTeachers();
+
+    Teacher findById(int id);
+
+    void updateTeacher(String name, String hireDate, String gender, String zipcode, Integer classesTaught, int id);
+
+    void deleteTeacherById(int id);
+
+    void removeAllTeachers();
+}
