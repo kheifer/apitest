4a3d077807dd3ab9b71f9e4e2a8779c56817dc68
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-08-25 10:02:49 -0700

    add Sql2oTrackDao file

---
+++ api-epicodus/src/main/java/dao/Sql2oTrackDao.java
@@ -0,0 +1,37 @@
+package dao;
+
+import dataSources.Track;
+
+import java.util.List;
+
+public class Sql2oTrackDao implements TrackDao {
+    @Override
+    public void add(Track newTrack) {
+
+    }
+
+    @Override
+    public void addTrackToCohort(int cohortId, int trackId) {
+
+    }
+
+    @Override
+    public List<Track> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Track> getAllTracksByCohort(int cohortId) {
+        return null;
+    }
+
+    @Override
+    public Track findById(int trackId) {
+        return null;
+    }
+
+    @Override
+    public void deleteTrack(int trackId) {
+
+    }
+}
---
+++ api-epicodus/src/test/java/dao/Sql2oTrackDaoTest.java
@@ -0,0 +1,42 @@
+package dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Sql2oTrackDaoTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void add() throws Exception {
+    }
+
+    @Test
+    public void addTrackToCohort() throws Exception {
+    }
+
+    @Test
+    public void getAll() throws Exception {
+    }
+
+    @Test
+    public void getAllTracksByCohort() throws Exception {
+    }
+
+    @Test
+    public void findById() throws Exception {
+    }
+
+    @Test
+    public void deleteTrack() throws Exception {
+    }
+
+}
