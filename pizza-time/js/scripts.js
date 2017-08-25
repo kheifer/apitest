c414d2f85db2e1daa39986bcb7999113c04f3047
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-08-25 14:26:50 -0700

    add files

--- pizza-time/js/scripts.js
+++
@@ -1,6 +0,0 @@
-function Pizza(pizzaSize, cheese, meatToppings, vegToppings) {
-  this.pizzaSize = pizzaSize;
-  this.cheese = cheese;
-  this.meatToppings = meatToppings;
-  this.vegToppings = vegToppings;
-}
