14563d889746becf35151344de3b61cd61904de9
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-08-25 10:01:24 -0700

    add Sql2oCohortDao file

---
+++ api-epicodus/src/main/java/dao/Sql2oCohortDao.java
@@ -0,0 +1,32 @@
+package dao;
+
+import dataSources.Cohort;
+
+import java.util.List;
+
+public class Sql2oCohortDao implements CohortDao {
+    @Override
+    public void add(Cohort cohort) {
+
+    }
+
+    @Override
+    public List<Cohort> getAll() {
+        return null;
+    }
+
+    @Override
+    public Cohort findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void updateCohort(String cohortName, String cohortLocation, int cohortId) {
+
+    }
+
+    @Override
+    public void deleteCohortById(int id) {
+
+    }
+}
---
+++ api-epicodus/src/test/java/dao/Sql2oCohortDaoTest.java
@@ -0,0 +1,38 @@
+package dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Sql2oCohortDaoTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void add() throws Exception {
+    }
+
+    @Test
+    public void getAll() throws Exception {
+    }
+
+    @Test
+    public void findById() throws Exception {
+    }
+
+    @Test
+    public void updateCohort() throws Exception {
+    }
+
+    @Test
+    public void deleteCohortById() throws Exception {
+    }
+
+}
