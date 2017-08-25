012cb0c5a6adba28fcc5e4bc978b8bb4fed42dd1
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-08-25 10:31:00 -0700

    add passing methods for Sql2oCohortDao

--- api-epicodus/src/main/java/dao/Sql2oCohortDao.java
+++ api-epicodus/src/main/java/dao/Sql2oCohortDao.java
@@ -30,21 +30,48 @@ public class Sql2oCohortDao implements CohortDao {
 
     @Override
     public List<Cohort> getAll() {
-        return null;
+        String query = "SELECT * FROM cohorts";
+        try(Connection con = sql2o.open()){
+            return con.createQuery(query)
+                    .executeAndFetch(Cohort.class);
+        }
     }
 
     @Override
     public Cohort findById(int id) {
-        return null;
+        String query = "SELECT * FROM cohorts WHERE cohortId = :cohortId";
+        try(Connection con = sql2o.open()){
+            return con.createQuery(query)
+                    .addParameter("cohortId", id)
+                    .executeAndFetchFirst(Cohort.class);
+        }
     }
 
     @Override
     public void updateCohort(String cohortName, String cohortLocation, String dateStarted, int cohortId) {
-
+        String query = "UPDATE cohorts SET(cohortName, location, dateStarted) = (:cohortName, :location, :dateStarted) WHERE cohortId = :cohortId";
+        try(Connection con = sql2o.open()){
+            con.createQuery(query)
+                    .addParameter("cohortName", cohortName)
+                    .addParameter("location", cohortLocation)
+                    .addParameter("dateStarted", dateStarted)
+                    .addParameter("cohortId", cohortId)
+                    .throwOnMappingFailure(false)
+                    .executeUpdate();
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+        }
     }
 
     @Override
     public void deleteCohortById(int id) {
-
+        String query = "DELETE FROM cohorts WHERE cohortId = :cohortId";
+        try(Connection con = sql2o.open()){
+            con.createQuery(query)
+                    .addParameter("cohortId", id)
+                    .executeUpdate();
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+        }
     }
 }
